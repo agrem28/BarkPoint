87fc65e98d3e6e8f40aeea123a39d6c6472e514e
benyoss <benyoss4@gmail.com>
2020-11-27 17:27:06 -0600

    (remove) console.log

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/barkpoint', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/barkpoint', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('we are connected');
+  console.warn('we are connected');
 });
 
 module.exports = db;
