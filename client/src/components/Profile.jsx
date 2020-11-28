7cde6f26015485fee5a99959fd2ef6bb24ba25cc
benyoss <benyoss4@gmail.com>
2020-11-28 16:36:17 -0600

    (fix) layouts of all files to meet eslint standard

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import DogComp from './DogComp';
 import './Profile.css';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { styled } from '@material-ui/core/styles';
+// import MenuIcon from '@material-ui/icons/Menu';
+
 import Button from '@material-ui/core/Button';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -16,11 +15,12 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
+// import Avatar from '@material-ui/core/Avatar';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import DogComp from './DogComp';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -48,10 +48,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 const Profile = () => {
-  const [dogs, setDogs] = React.useState([{name: 'Fido', image: 'https://i.pinimg.com/originals/d1/8c/ac/d18cacc593eff679f63fa49e265a7ade.jpg'},
-  {name: 'Mickey', image: 'https://i.insider.com/5c005d9bac00e20fe169f725?width=1100&format=jpeg&auto=webp'}, {name: 'Marc Anthony', image: 'https://www.rover.com/blog/wp-content/uploads/2020/01/White-Dog-in-Vest-and-Sun-Goggles-960x540.jpg'}])
+  const [dogs] = React.useState([{ name: 'Fido', image: 'https://i.pinimg.com/originals/d1/8c/ac/d18cacc593eff679f63fa49e265a7ade.jpg' },
+    { name: 'Mickey', image: 'https://i.insider.com/5c005d9bac00e20fe169f725?width=1100&format=jpeg&auto=webp' }, { name: 'Marc Anthony', image: 'https://www.rover.com/blog/wp-content/uploads/2020/01/White-Dog-in-Vest-and-Sun-Goggles-960x540.jpg' }]);
   const [expanded, setExpanded] = React.useState(false);
 
   const classes = useStyles();
@@ -62,12 +61,12 @@ const Profile = () => {
 
   const styles = {
     root: {
-      flexGrow: 1
+      flexGrow: 1,
     },
     typography: {
       flexGrow: 1,
-      align: "center"
-    }
+      align: 'center',
+    },
   };
 
   const MyButton = styled(Button)({
@@ -82,20 +81,23 @@ const Profile = () => {
   });
 
   return (
-    <div className='Profile'>
-    <div className={classes.root}>
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <Typography variant="h6" color="#FEFDFF" style={styles.typography} className='myDogs'>
-          NAV BAR WILL GO HERE
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </div>
-    <div className='dogs' style={{width: '100%', height: '100%'}}>
-      {dogs.map((dog, i) => (
-            <div className='header' style={{paddingBottom: '10px'}}>
-            <Card style={{Width: '50%', height: 'auto', flexDirection: 'row', backgroundColor: "#FEFDFF", breakInside: 'avoid', fontFamily: 'Roboto'}}>
+    <div className="Profile">
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="#FEFDFF" style={styles.typography} className="myDogs">
+              NAV BAR WILL GO HERE
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div className="dogs" style={{ width: '100%', height: '100%' }}>
+        {dogs.map((dog) => (
+          <div className="header" style={{ paddingBottom: '10px' }}>
+            <Card style={{
+              Width: '50%', height: 'auto', flexDirection: 'row', backgroundColor: '#FEFDFF', breakInside: 'avoid', fontFamily: 'Roboto',
+            }}
+            >
               <CardHeader
                 title={dog.name}
               />
@@ -104,7 +106,7 @@ const Profile = () => {
                 image={dog.image}
               />
               <CardActions disableSpacing>
-              <h3>{`${dog.name}'s Toys`}</h3>
+                <h3>{`${dog.name}'s Toys`}</h3>
                 <IconButton
                   className={clsx(classes.expand, {
                     [classes.expandOpen]: expanded,
@@ -122,15 +124,17 @@ const Profile = () => {
                 </CardContent>
               </Collapse>
             </Card>
-            </div>
-      ))}
-    </div>
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <MyButton>Add Dog</MyButton>
-    </div>
+          </div>
+        ))}
+      </div>
+      <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+      }}
+      >
+        <MyButton>Add Dog</MyButton>
+      </div>
     </div>
   );
-}
+};
 
 export default Profile;
-
