84de93c3bef236879979f58b417ae5b0702915d0
Jazmine <jazminejfox@gmail.com>
2020-11-28 12:14:36 -0600

    (create) profile.css page, dogcomp page, toybox

---
+++ client/src/components/ToyBox.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import ToyList from './ToyList';
+
+const ToyBox = () => {
+  const hi = 'hi';
+
+  const getDummyData = async => {
+    
+  };
+
+  return (
+    <div>
+      {hi}
+    </div>
+  );
+};
+
+export default ToyBox;
