94c3b57e82daea703fac92a9720aa57db5b4fc50
benyoss <benyoss4@gmail.com>
2020-11-27 19:02:25 -0600

    (fix) rebase merge conflict

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,15 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
   name: String,
   email: String,
+<<<<<<< HEAD
+=======
+  image: String,
+>>>>>>> (fix) format in model files
 });
 
 const User = model('User', userSchema);
 
+<<<<<<< HEAD
 const createUser = (body) => {
   User.create({
     name: body.name,
@@ -15,3 +20,6 @@ const createUser = (body) => {
 };
 
 module.exports = createUser;
+=======
+module.exports = User;
+>>>>>>> (fix) format in model files
