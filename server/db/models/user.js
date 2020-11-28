4a1b4d0bc5705b22d96a65109f5212502c1768f1
benyoss <benyoss4@gmail.com>
2020-11-28 15:17:56 -0600

    (fix) merge conflict error

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,15 +3,29 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
   name: String,
   email: String,
+  thumbnail: String,
 });
 
 const User = model('User', userSchema);
 
 const createUser = (body) => {
-  User.create({
+  const user = new User({
     name: body.name,
     email: body.email,
+    thumbnail: body.picture,
+  });
+  const { _id } = user;
+  return User.findOne({ _id }).then((result) => {
+    if (!result) {
+      return User.create(user);
+    }
+    return '';
   });
 };
 
-module.exports = createUser;
+const findUser = (username) => User.findOne({ name: username }).exec();
+
+module.exports = {
+  createUser,
+  findUser,
+};
