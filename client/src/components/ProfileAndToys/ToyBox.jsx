8893f160a2ffff9e8579c6e684443d93bf5b1339
Aiesha <abailey92@DESKTOP-0NID363.localdomain>
2020-12-05 10:47:41 -0600

    (update) styling on all components and (update) package.json file

--- client/src/components/ProfileAndToys/ToyBox.jsx
+++ client/src/components/ProfileAndToys/ToyBox.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 
 // Button Import
 import Button from '@material-ui/core/Button';
-import { makeStyles, styled } from '@material-ui/core/styles';
+import { makeStyles, styled, withStyles } from '@material-ui/core/styles';
 
 // Card Import
 import Card from '@material-ui/core/Card';
@@ -17,8 +17,13 @@ import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PetsIcon from '@material-ui/icons/Pets';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
 import Navbar from './Navbar';
-// import Profile from './Profile';
 
 const axios = require('axios');
 
@@ -34,8 +39,10 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '20px',
   },
   media: {
-    height: '20px',
-    paddingTop: '60px',
+    height: '40vh',
+    width: '20vw',
+    maxHeight: '40vh',
+    cursor: 'pointer',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -52,11 +59,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const StyledRating = withStyles({
+  iconFilled: {
+    color: '#0E4749',
+  },
+})(Rating);
+
 const ToyBox = ({ dogs, getDogs }) => {
   const classes = useStyles();
   const [toys, setToys] = useState([]);
   const [hide, setHide] = useState(true);
   const [theCurrentDog, settheCurrentDog] = useState(dogs[0]);
+  const [isClicked, setIsClicked] = useState({});
 
   const getToy = async () => {
     const { data } = await axios.get('session');
@@ -132,7 +146,7 @@ const ToyBox = ({ dogs, getDogs }) => {
   }, []);
 
   return (
-    <div>
+    <div className="background">
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <img
           src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F238583%2Fscreenshots%2F3630870%2Flagif-grande.gif"
@@ -161,6 +175,7 @@ const ToyBox = ({ dogs, getDogs }) => {
               flex: 1, marginRight: 'auto', height: '48', width: '200',
             }}
           >
+
             <select
               name="select"
               className="filter"
@@ -177,13 +192,14 @@ const ToyBox = ({ dogs, getDogs }) => {
         </div>
         <div
           className="dogs"
-          style={{
-            width: '100%', height: '100%', paddingBottom: '10px', columnCount: '3',
-          }}
+          // style={{
+          //   width: '100%', height: '100%', paddingBottom: '10px', columnCount: '3',
+          // }}
         >
-          {toys.slice(0, 6).map((toy) => (
+          { toys.slice(0, 6).map((toy) => (
             <Card
               className={classes.root}
+              style={{ border: '5px solid #002626' }}
             >
               <CardMedia
                 className={classes.media}
@@ -191,28 +207,50 @@ const ToyBox = ({ dogs, getDogs }) => {
                 onClick={() => { window.open(`${toy.link}`); }}
               />
               <CardContent>
-                <small>
+                <small style={{ fontFamily: 'Lato' }}>
                   {toy.title}
                 </small>
-                <p>
+                <p style={{ color: '#2CDA9D', fontFamily: 'Lato', fontSize: '18px' }}>
                   $
                   {toy.price.value}
                 </p>
               </CardContent>
               <CardActions disableSpacing>
-                <div>
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon onClick={() => {
-                      saveToy(toy.image, toy.title, toy.link, toy.rating, toy.price.value);
-                    }}
-                    />
-                  </IconButton>
-                </div>
-                <p>
-                  {toy.rating}
-                  {' '}
-                  Rating
-                </p>
+
+                <IconButton
+                  aria-label="add to favorites"
+                  onClick={() => {
+                    setIsClicked({ ...isClicked, [toy.asin]: !isClicked[toy.asin] });
+                    saveToy(toy.image, toy.title, toy.link, toy.rating, toy.price.value);
+                  }}
+                >
+                  { !isClicked[toy.asin]
+                    ? (
+                      <FavoriteBorderIcon
+                        fontSize="small"
+                        style={{ color: '#e55812' }}
+                      />
+                    )
+                    : (
+                      <FavoriteIcon
+                        fontSize="small"
+                        style={{ color: '#e55812' }}
+                      />
+                    )}
+                </IconButton>
+
+                <Box fontWeight={800} component="fieldset" mb={3} borderColor="transparent">
+                  <Typography variant="h5">Rating</Typography>
+                  <StyledRating
+                    name="customized-color"
+                    value={toy.rating}
+                    getLabelText={(value) => `${value} paws${value !== 1 ? 's' : ''}`}
+                    precision={0.1}
+                    readOnly
+                    icon={<PetsIcon fontSize="inherit" />}
+                  />
+                </Box>
+
               </CardActions>
             </Card>
           ))}
