17e1b9794f7af77fd0b2afe2cb6c015d9595b00f
Billy <nbilly319@gmail.com>
2020-12-07 19:45:39 -0600

    (update)

--- client/src/components/ProfileAndToys/FriendsList.jsx
+++ client/src/components/ProfileAndToys/FriendsList.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
-const FriendsList = () => {
-  console.log('Hey');
-  return (
-    <div className="Profile">
-      <Navbar />
-      <Sidebar />
-      <h1>This is your Friends list.</h1>
+const FriendsList = () => (
+  <div className="Profile">
+    <Navbar />
+    <Sidebar />
+    <div className="center">
+      <input />
+      <div className="friends">
+        <h1>This is your Friends list.</h1>
+      </div>
+      <div className="messages">
+        <h1>this is the messages</h1>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default FriendsList;
