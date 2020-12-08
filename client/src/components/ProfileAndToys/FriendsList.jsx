f60499218f27e0e0d37fc14bf7af4ab183cdda69
Billy <nbilly319@gmail.com>
2020-12-08 05:25:12 -0600

    (update) got the friend box and message box to go side by side

--- client/src/components/ProfileAndToys/FriendsList.jsx
+++ client/src/components/ProfileAndToys/FriendsList.jsx
@@ -7,12 +7,20 @@ const FriendsList = () => (
     <Navbar />
     <Sidebar />
     <div className="center">
+      <h4>search for dogs</h4>
       <input />
-      <div className="friends">
-        <h1>This is your Friends list.</h1>
-      </div>
-      <div className="messages">
-        <h1>this is the messages</h1>
+      <div className="main">
+        <div className="friends">
+          <h1>tom</h1>
+          <h1>ben</h1>
+          <h1>tim</h1>
+        </div>
+        <div className="messages">
+          <h3>tom</h3>
+          <div>heyy</div>
+          <h3>billy</h3>
+          <div>whats up man</div>
+        </div>
       </div>
     </div>
   </div>
