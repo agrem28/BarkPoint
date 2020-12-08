c21ea37386c937d13cc1a54e6286d1e0c6bc11b0
Billy <nbilly319@gmail.com>
2020-12-08 06:15:34 -0600

    (update) used dummy data to set up messages and have front end functional

--- client/src/components/ProfileAndToys/FriendsList.jsx
+++ client/src/components/ProfileAndToys/FriendsList.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
-const FriendsList = () => (
-  <div className="Profile">
-    <Navbar />
-    <Sidebar />
-    <div className="center">
-      <h4>search for dogs</h4>
-      <input />
-      <div className="main">
-        <div className="friends">
-          <h1>tom</h1>
-          <h1>ben</h1>
-          <h1>tim</h1>
-        </div>
-        <div className="messages">
-          <h3>tom</h3>
-          <div>heyy</div>
-          <h3>billy</h3>
-          <div>whats up man</div>
+const FriendsList = () => {
+  const [currentDms, setCurrentDms] = useState('Tee');
+  const [messageText, setMessageText] = useState('');
+  const exampleUser = ['Tee', 'Amber', 'Andrew'];
+  let exampleMessage = {
+    Tee: [{ name: 'Tee', message: 'hey man how your day' }, { name: 'Billy', message: 'its been good' }],
+    Amber: [{ name: 'Amber', message: 'does the site looks good' }, { name: 'Billy', message: 'sure does' }],
+    Andrew: [{ name: 'Andrew', message: 'i like this youtube video' }, { name: 'Billy', message: 'same' }],
+  };
+  const [messages, setMessages] = useState(exampleMessage);
+  return (
+    <div className="Profile">
+      <Navbar />
+      <Sidebar />
+      <div className="center">
+        <h4>search for dogs</h4>
+        <input />
+        <div className="main">
+          <div className="friends">
+            {exampleUser.map((user) => (<h1 onClick={() => setCurrentDms(user)}>{user}</h1>))}
+          </div>
+          <div className="messages">
+            {
+            messages[currentDms].map(({ name, message }) => (
+              <div>
+                <h2>{name}</h2>
+                <div>{message}</div>
+              </div>
+            ))
+            }
+            <input placeholder="type a message" value={messageText} onChange={(e) => setMessageText(e.target.value)} />
+            <button onClick={() => {
+              const newMessage = {
+                name: 'Billy',
+                message: messageText,
+              };
+              exampleMessage = messages;
+              exampleMessage[currentDms] = [...messages[currentDms], newMessage];
+              setMessages(exampleMessage);
+              setMessageText('');
+            }}
+            >
+              send message
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default FriendsList;
